b264b96c2fa2c2b5fe2d0dbbf3f0409a804eef39
washluis-alencar <wluisalencar@gmail.com>
2025-09-19 11:28:02 -0300

    feat(eng-8377): adding tests to covert request/response transforms

--- internal/provider/resource_basistheory_proxy_test.go
+++ internal/provider/resource_basistheory_proxy_test.go
@@ -451,6 +451,225 @@ func TestResourceProxyWithTokenizeRequestTransform(t *testing.T) {
 	})
 }
 
+func TestResourceProxyWithTwoRequestTransforms(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		CheckDestroy:      testAccCheckProxyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: buildProxyWithTwoRequestTransforms(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Check first transform (code)
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "request_transforms.0.type", "code"),
+					resource.TestMatchResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "request_transforms.0.code", regexp.MustCompile("module.exports = async function")),
+					// Check second transform (tokenize)
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "request_transforms.1.type", "tokenize"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "request_transforms.1.options.identifier", "outputTokenB"),
+					// Check that token is a valid JSON string containing the expected structure
+					func(s *terraform.State) error {
+						rs, ok := s.RootModule().Resources["basistheory_proxy.terraform_test_proxy"]
+						if !ok {
+							return fmt.Errorf("resource not found: basistheory_proxy.terraform_test_proxy")
+						}
+
+						tokenValue, ok := rs.Primary.Attributes["request_transforms.1.options.token"]
+						if !ok {
+							return fmt.Errorf("token attribute not found")
+						}
+
+						var token map[string]interface{}
+						if err := json.Unmarshal([]byte(tokenValue), &token); err != nil {
+							return fmt.Errorf("token is not valid JSON: %v", err)
+						}
+
+						if token["type"] != "token" {
+							return fmt.Errorf("expected token.type to be 'token', got %v", token["type"])
+						}
+						if token["data"] != "{{ encrypted | json: '$.data' }}" {
+							return fmt.Errorf("expected token.data to be '{{ encrypted | json: '$.data' }}', got %v", token["data"])
+						}
+
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
+
+func TestResourceProxyWithMultipleResponseTransforms(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: buildProxyWithMultipleResponseTransforms(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "name", "Test proxy"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "destination_url", "https://echo.basistheory.com/anything"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "require_auth", "false"),
+					// Check first response transform (tokenize)
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "response_transforms.0.type", "tokenize"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "response_transforms.0.options.identifier", "cardToken"),
+					// Check second response transform (append_json)
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "response_transforms.1.type", "append_json"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "response_transforms.1.options.value", "{{ transform_identifier: 'cardToken' | json: '$.id' }}"),
+					resource.TestCheckResourceAttr(
+						"basistheory_proxy.terraform_test_proxy", "response_transforms.1.options.location", "$.card_token_id"),
+					// Check that token is a valid JSON string containing the expected structure
+					func(s *terraform.State) error {
+						rs, ok := s.RootModule().Resources["basistheory_proxy.terraform_test_proxy"]
+						if !ok {
+							return fmt.Errorf("resource not found: basistheory_proxy.terraform_test_proxy")
+						}
+
+						tokenValue, ok := rs.Primary.Attributes["response_transforms.0.options.token"]
+						if !ok {
+							return fmt.Errorf("token attribute not found")
+						}
+
+						var token map[string]interface{}
+						if err := json.Unmarshal([]byte(tokenValue), &token); err != nil {
+							return fmt.Errorf("token is not valid JSON: %v", err)
+						}
+
+						if token["type"] != "card" {
+							return fmt.Errorf("expected token.type to be 'card', got %v", token["type"])
+						}
+						if token["data"] != "{{ res.json }}" {
+							return fmt.Errorf("expected token.data to be '{{ res.json }}', got %v", token["data"])
+						}
+
+						if metadata, ok := token["metadata"].(map[string]interface{}); ok {
+							if metadata["source"] != "proxy-response-transform" {
+								return fmt.Errorf("expected metadata.source to be 'proxy-response-transform', got %v", metadata["source"])
+							}
+							if metadata["number"] != "123" {
+								return fmt.Errorf("expected metadata.number to be '123', got %v", metadata["number"])
+							}
+						} else {
+							return fmt.Errorf("expected Metadata to be an object, got %v", token["Metadata"])
+						}
+
+						return nil
+					},
+				),
+			},
+		},
+	})
+}
+
+func TestResourceProxyMultipleCodeTransforms(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config:      buildProxyWithMultipleCodeTransforms(),
+				ExpectError: regexp.MustCompile(`only one CODE transform is allowed`),
+			},
+		},
+	})
+}
+
+func TestResourceProxyDuplicateTokenizeIdentifiers(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config:      buildProxyWithDuplicateIdentifiers(),
+				ExpectError: regexp.MustCompile(`duplicate identifier found.*duplicateId`),
+			},
+		},
+	})
+}
+
+func TestResourceProxyTokenizeValidationErrors(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config:      buildProxyWithTokenizeNoToken(),
+				ExpectError: regexp.MustCompile(`token is required in tokenize transform options`),
+			},
+			{
+				Config:      buildProxyWithTokenizeInvalidJSON(),
+				ExpectError: regexp.MustCompile(`token must be valid JSON`),
+			},
+		},
+	})
+}
+
+func TestResourceProxyAppendTransformValidations(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config:      buildProxyWithAppendTransformNoValue(),
+				ExpectError: regexp.MustCompile(`value is required in append transform options`),
+			},
+			{
+				Config:      buildProxyWithAppendJSONNoLocation(),
+				ExpectError: regexp.MustCompile(`location is required for append_json transforms`),
+			},
+			{
+				Config:      buildProxyWithAppendHeaderNoLocation(),
+				ExpectError: regexp.MustCompile(`location is required for append_header transforms`),
+			},
+		},
+	})
+}
+
+func TestResourceProxyMaskEdgeCases(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { preCheck(t) },
+		ProviderFactories: getProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config:      buildProxyWithMaskChaseStratusAndExpression(),
+				ExpectError: regexp.MustCompile(`(?s)expression should not be provided when matcher is.*'chase_stratus_pan'`),
+			},
+		},
+	})
+}
+
+func testAccCheckProxyDestroy(state *terraform.State) error {
+	basisTheoryClient := basistheoryClient.NewClient(
+		option.WithAPIKey(os.Getenv("BASISTHEORY_API_KEY")),
+		option.WithBaseURL(os.Getenv("BASISTHEORY_API_URL")),
+	)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "basistheory_proxy" {
+			continue
+		}
+
+		_, err := basisTheoryClient.Proxies.Get(context.TODO(), rs.Primary.ID)
+
+		var notFoundError basistheory.NotFoundError
+		if errors.As(err, &notFoundError) {
+			return err
+		}
+	}
+
+	return nil
+}
+
 const testAccProxyCreate = `
 resource "basistheory_proxy" "terraform_test_proxy" {
   name = "Terraform proxy"
@@ -586,28 +805,6 @@ resource "basistheory_proxy" "terraform_test_proxy" {
 }
 `
 
-func testAccCheckProxyDestroy(state *terraform.State) error {
-	basisTheoryClient := basistheoryClient.NewClient(
-		option.WithAPIKey(os.Getenv("BASISTHEORY_API_KEY")),
-		option.WithBaseURL(os.Getenv("BASISTHEORY_API_URL")),
-	)
-
-	for _, rs := range state.RootModule().Resources {
-		if rs.Type != "basistheory_proxy" {
-			continue
-		}
-
-		_, err := basisTheoryClient.Proxies.Get(context.TODO(), rs.Primary.ID)
-
-		var notFoundError basistheory.NotFoundError
-		if errors.As(err, &notFoundError) {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func buildProxyWithResponseTransformAttributes(config string) string {
 	return fmt.Sprintf(`
 resource "basistheory_proxy" "terraform_test_proxy" {
@@ -632,4 +829,216 @@ resource "basistheory_proxy" "terraform_test_proxy" {
   require_auth = false
 }
 `, config)
+}
+
+func buildProxyWithTwoRequestTransforms() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Terraform proxy"
+  destination_url = "https://httpbin.org/post"
+  request_transforms {
+    type = "code"
+    code = <<-EOT
+              module.exports = async function (context) {
+                return context;
+              };
+          EOT
+  }
+  request_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "outputTokenB"
+      token = jsonencode({
+        type = "token"
+        data = "{{ encrypted | json: '$.data' }}"
+      })
+    }
+  }
+  require_auth = false
+}
+`
+}
+
+func buildProxyWithMultipleResponseTransforms() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://echo.basistheory.com/anything"
+  require_auth = false
+  response_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "cardToken"
+      token = jsonencode({
+        type = "card"
+        data = "{{ res.json }}"
+        metadata = {
+          source = "proxy-response-transform"
+          number = "123"
+        }
+      })
+    }
+  }
+  response_transforms {
+    type = "append_json"
+    options = {
+      value = "{{ transform_identifier: 'cardToken' | json: '$.id' }}"
+      location = "$.card_token_id"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithMultipleCodeTransforms() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  request_transforms {
+    type = "code"
+    code = <<-EOT
+              module.exports = async function (context) {
+                return context;
+              };
+          EOT
+  }
+  request_transforms {
+    type = "code"
+    code = <<-EOT
+              module.exports = async function (context) {
+                return context;
+              };
+          EOT
+  }
+}
+`
+}
+
+func buildProxyWithDuplicateIdentifiers() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  request_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "duplicateId"
+      token = jsonencode({
+        type = "card"
+        data = "{{ request | json: '$.card1' }}"
+      })
+    }
+  }
+  request_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "duplicateId"
+      token = jsonencode({
+        type = "token"
+        data = "{{ request | json: '$.card2' }}"
+      })
+    }
+  }
+}
+`
+}
+
+func buildProxyWithTokenizeNoToken() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  request_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "testId"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithTokenizeInvalidJSON() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  request_transforms {
+    type = "tokenize"
+    options = {
+      identifier = "testId"
+      token = "invalid-json-string"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithAppendTransformNoValue() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  response_transforms {
+    type = "append_text"
+    options = {
+      location = "$.test"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithAppendJSONNoLocation() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  response_transforms {
+    type = "append_json"
+    options = {
+      value = "test value"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithAppendHeaderNoLocation() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  response_transforms {
+    type = "append_header"
+    options = {
+      value = "test value"
+    }
+  }
+}
+`
+}
+
+func buildProxyWithMaskChaseStratusAndExpression() string {
+	return `
+resource "basistheory_proxy" "terraform_test_proxy" {
+  name = "Test proxy"
+  destination_url = "https://httpbin.org/post"
+  require_auth = false
+  response_transforms {
+    type = "mask"
+    matcher = "chase_stratus_pan"
+    expression = "(.*)"
+    replacement = "*"
+  }
+}
+`
 }
